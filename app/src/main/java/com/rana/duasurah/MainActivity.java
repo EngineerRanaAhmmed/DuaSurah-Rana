d69c495f683bb277ca8368ec4e50620ed5c76d54
Md Sharif Mia <46758894+SharifMia@users.noreply.github.com>
2021-03-06 20:08:31 +0600

    Font family update

--- app/src/main/java/com/rana/duasurah/MainActivity.java
+++ app/src/main/java/com/rana/duasurah/MainActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
