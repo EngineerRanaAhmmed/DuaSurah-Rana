d08e063d2febdb4f91979289fd43b167d2f4ffc5
Md Sharif Mia <46758894+SharifMia@users.noreply.github.com>
2021-03-15 15:11:12 +0600

    ads okay

--- app/src/main/java/com/rana/duasurah/MainActivity.java
+++ app/src/main/java/com/rana/duasurah/MainActivity.java
@@ -1,12 +1,20 @@
 package com.rana.duasurah;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.rewarded.RewardItem;
+import com.google.android.gms.ads.rewarded.RewardedAd;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -16,15 +24,80 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.gms.ads.rewarded.RewardedAdCallback;
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private DrawerLayout drawer;
+    private RewardedAd rewardedAd;
+    private InterstitialAd mInterstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mInterstitialAd = new InterstitialAd(MainActivity.this);
+        mInterstitialAd.setAdUnitId(getString(R.string.Ins_ad_unit_id));
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mInterstitialAd.loadAd(adRequest);
+
+        rewardedAd = new RewardedAd(this,
+                getString(R.string.Rewarded_ad_unit_id));
+
+
+        RewardedAdLoadCallback adLoadCallback = new RewardedAdLoadCallback() {
+            @Override
+            public void onRewardedAdLoaded() {
+                // Ad successfully loaded.
+            }
+
+            @Override
+            public void onRewardedAdFailedToLoad(int errorCode) {
+                // Ad failed to load.
+            }
+        };
+        rewardedAd.loadAd(new AdRequest.Builder().build(), adLoadCallback);
+
+
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                if (rewardedAd.isLoaded()) {
+                    Activity activityContext = MainActivity.this;
+                    RewardedAdCallback adCallback = new RewardedAdCallback() {
+                        @Override
+                        public void onRewardedAdOpened() {
+                            // Ad opened.
+                        }
+
+                        @Override
+                        public void onRewardedAdClosed() {
+                            // Ad closed.
+                        }
+
+                        @Override
+                        public void onUserEarnedReward(@NonNull RewardItem reward) {
+                            // User earned reward.
+                        }
+
+                        @Override
+                        public void onRewardedAdFailedToShow(int errorCode) {
+                            // Ad failed to display.
+                            Ads();
+                        }
+                    };
+                    rewardedAd.show(activityContext, adCallback);
+                } else {
+                    Log.d("TAG", "The rewarded ad wasn't loaded yet.");
+                }
+
+            }
+        },5000);
 
 
 
@@ -51,6 +124,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         if (savedInstanceState == null){
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
             navigationView.setCheckedItem(R.id.nav_home);
+
+
+
         }
 
     }
@@ -83,31 +159,38 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawer.closeDrawer(GravityCompat.START);
         switch (item.getItemId()){
             case R.id.nav_home:
+                Ads();
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
                 break;
 
             case R.id.nav_home2:
+                Ads();
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new DuaFragment()).commit();
                 break;
 
             case R.id.nav_home3:
+                Ads();
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new NamezFragment()).commit();
                 break;
 
             case R.id.nav_home4:
+                Ads();
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new IslamicStatusFragment()).commit();
                 break;
 
             case R.id.nav_contact:
+                Ads();
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new ContactFragment()).commit();
                 break;
             case R.id.nav_rateBar:
+                Ads();
                 Uri uri = Uri.parse("https://play.google.com/store/apps/details?id=" + getPackageName());
                 Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(intent);
                 break;
             case R.id.nav_share:
             case R.id.nav_send:
+                Ads();
                 String myData = "https://play.google.com/store/apps/details?id=" + getPackageName();
                 Intent intentShare = new Intent();
                 intentShare.setAction(Intent.ACTION_SEND);
@@ -145,6 +228,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
         return super.onOptionsItemSelected(item);
 
+    }
+
+
+    private void Ads(){
+        if (mInterstitialAd.isLoaded()){
+            mInterstitialAd.show();
+        }
     }
 
 
