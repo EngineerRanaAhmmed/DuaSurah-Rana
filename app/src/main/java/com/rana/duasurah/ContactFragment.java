5c374ab6930b3b7e355c0a6c849a6db63ce3a9f5
Md Sharif Mia <46758894+SharifMia@users.noreply.github.com>
2021-03-21 15:32:42 +0600

    diner update

--- app/src/main/java/com/rana/duasurah/ContactFragment.java
+++ app/src/main/java/com/rana/duasurah/ContactFragment.java
@@ -1,17 +1,13 @@
 package com.rana.duasurah;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class ContactFragment extends Fragment {
-    TextView whatsaApp;
 
 
     @Override
@@ -19,12 +15,6 @@ public class ContactFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_contact, container, false);
 
-        whatsaApp = view.findViewById(R.id.whatsAppLink);
-
-        whatsaApp.setOnClickListener(v -> {
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://wa.me/8801947781499"));
-            startActivity(intent);
-        });
 
         return view;
     }
--- app/src/main/java/com/rana/duasurah/HomeFragment.java
+++ app/src/main/java/com/rana/duasurah/HomeFragment.java
@@ -6,9 +6,11 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class HomeFragment extends Fragment {
 
@@ -17,6 +19,10 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        TextView textView = view.findViewById(R.id.marqueeText);
+        textView.setSelected(true);
 
 
         String[] sms = getResources().getStringArray(R.array.button_name);
